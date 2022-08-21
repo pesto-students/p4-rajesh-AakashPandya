603f1650123342856a498334204a0ed63cc148d3
Aakash Pandya <apandya@mobiquityinc.com>
2022-08-21 09:55:43 +0530

    covered test cases in case argument/s are not passed

--- Week-5/Exercise-5.4/calculator.test.js
+++ Week-5/Exercise-5.4/calculator.test.js
@@ -6,6 +6,14 @@ describe("Calculator tests", () => {
     expect(result).toBe(9);
   });
 
+  test("calling sum method without passing arguments", () => {
+    const result = mathOperations.sum();
+    expect(result).toBe(NaN);
+
+    const result2 = mathOperations.sum(1);
+    expect(result2).toBe(NaN);
+  });
+
   test("adding -4 and 5 should return 1", () => {
     const result = mathOperations.sum(-4, 5);
     expect(result).toBe(1);
@@ -23,9 +31,25 @@ describe("Calculator tests", () => {
     expect(result).toBe(12);
   });
 
+  test("calling subtract method without passing arguments", () => {
+    const result = mathOperations.diff();
+    expect(result).toBe(NaN);
+
+    const result2 = mathOperations.diff(1);
+    expect(result2).toBe(NaN);
+  });
+
   test("multiplying 2 and 8 should return 16", () => {
     const result = mathOperations.product(2, 8);
 
     expect(result).toBe(16);
+  });
+
+  test("calling multiply method without passing arguments", () => {
+    const result = mathOperations.product();
+    expect(result).toBe(NaN);
+
+    const result2 = mathOperations.product(1);
+    expect(result2).toBe(NaN);
   });
 });
