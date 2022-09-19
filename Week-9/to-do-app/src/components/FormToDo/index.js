b6000995deaf54423f4dd704fd8cd9c622c8b7eb
Aakash Pandya <apandya@mobiquityinc.com>
2022-09-19 14:00:05 +0530

    app init

---
+++ Week-9/to-do-app/src/components/FormToDo/index.js
@@ -0,0 +1,32 @@
+const FormTodo = ({ addTodo }) => {
+  const [value, setValue] = React.useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!value) return;
+    addTodo(value);
+    setValue("");
+  };
+
+  return (
+    <Form onSubmit={handleSubmit}>
+      <Form.Group>
+        <Form.Label>
+          <b>Add Todo</b>
+        </Form.Label>
+        <Form.Control
+          type="text"
+          className="input"
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+          placeholder="Add new todo"
+        />
+      </Form.Group>
+      <Button variant="primary mb-3" type="submit">
+        Submit
+      </Button>
+    </Form>
+  );
+};
+
+export default FormToDo;
---
+++ Week-9/to-do-app/src/components/ToDoList/index.js
@@ -0,0 +1,56 @@
+import { Card } from "react-bootstrap";
+import FormTodo from "../FormTodo";
+import TodoListItem from "../ToDoListItem";
+import { useState } from "react";
+
+const ToDoList = () => {
+  const [todos, setTodos] = useState([
+    {
+      text: "This is a sampe todo",
+      isDone: false,
+    },
+  ]);
+
+  const addTodo = (text) => {
+    const newTodos = [...todos, { text }];
+    setTodos(newTodos);
+  };
+
+  const markTodo = (index) => {
+    const newTodos = [...todos];
+    newTodos[index].isDone = true;
+    setTodos(newTodos);
+  };
+
+  const removeTodo = (index) => {
+    const newTodos = [...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+  };
+
+  return (
+    <div className="app">
+      <div className="container">
+        <h1 className="text-center mb-4">Todo List</h1>
+        <FormTodo addTodo={addTodo} />
+        <div>
+          {todos.map((todo, index) => (
+            <Card>
+              <Card.Body>
+                <TodoListItem
+                  key={index}
+                  index={index}
+                  todo={todo}
+                  markTodo={markTodo}
+                  removeTodo={removeTodo}
+                />
+              </Card.Body>
+            </Card>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ToDoList;
